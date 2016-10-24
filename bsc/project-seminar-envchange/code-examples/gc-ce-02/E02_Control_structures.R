7de843f6924fcc3ceaff2cd9381531ddf153c01a
tim-salabim <tim.appelhans@gmail.com>
2016-10-24 18:09:03 +0200

    added new versioning

---
+++ bsc/project-seminar-envchange/code-examples/gc-ce-02/E02_Control_structures.R
@@ -0,0 +1,21 @@
+#' ---
+#' title:
+#' author: Tim Appelhans & Hanna Meyer
+#' ---
+
+#+ setup, include=FALSE
+knitr::opts_knit$set(
+  root.dir = '/home/ede/tappelhans/uni/marburg/lehre/2016/ss/PS_global_change')
+
+#'
+#' ###
+#'
+library(rgdal)
+
+#' first we set our working directory
+#+ wd, eval = FALSE
+
+setwd('/home/ede/tappelhans/uni/marburg/lehre/2016/ss/PS_global_change')
+
+#' then we can start working
+
